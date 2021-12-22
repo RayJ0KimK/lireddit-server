5182999e474f0a573c2a7da73a7d1f0b161b7aa7
ricky <dehao@bu.edu>
2021-12-22 01:32:08 -0800

    <fix>: Fixed transpile issue of node-redis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import { MyContext } from "./types";
 import {MikroORM} from "@mikro-orm/core";
 import express from "express";
 import {__prod__} from "./constants";
@@ -8,6 +9,11 @@ import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from "type-graphql";
 import {HelloResolver} from "./resolvers/hello";
 import {PostResolver} from "./resolvers/post"
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -15,18 +21,45 @@ const main = async () => {
 
     const app = express();
 
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient()
+
+    app.use(
+        session({
+            name:'qid',
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60, // one hour
+                httpOnly: true,
+                sameSite:'lax', //csrf
+                secure: __prod__ , // Cookie only works in https
+            },
+            saveUninitialized: false,
+            secret: 'keyboard penis',
+            resave: false,
+        })
+    )
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em})
-    })    
+        //this is where em was initialized.
+        context: ({req, res}): MyContext => ({em: orm.em, req, res}),
+    });
 
     app.get('/', (_, res) => {
         res.send('hello');
     })
+    
     await apolloServer.start()
+    //Apollo server initiates a middelware on app, which is express, 
+    // and the middleware will find the context in the apolloServer
+    // So how did shenlearn handle its middleware in the past ?
     apolloServer.applyMiddleware({app})
     
     app.listen(4000, () => {
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,7 @@
 import { MyContext } from "../types";
-import {Arg, Ctx, Resolver, InputType, Field, Mutation} from "type-graphql";
+import {Arg, Ctx, Resolver, InputType, ObjectType, Field, Mutation} from "type-graphql";
 import {User} from "../entities/User";
+import argon2 from 'argon2';
 
 @InputType()
 class UsernamePasswordInput {
@@ -10,15 +11,80 @@ class UsernamePasswordInput {
     password: string
 }
 
+@ObjectType()
+class FieldError{
+    @Field()
+    field: string;
+    
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError [];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
+}
+
 @Resolver()
 export class UserResolver{
-    @Mutation(() => String)
+    //The thing inside the parenthesis is some type of config of this operation
+    @Mutation(() => User)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx(){em}: MyContext
     ){
-        const user = em.create(User, {username: options.username})
+        const hasedPassword = await argon2.hash(options.password);
+        const user = em.create(User, {username: options.username, password: hasedPassword});
         await em.persistAndFlush(user)
-        return "bye";
+        return user;
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx(){em, req}: MyContext
+    ): Promise<UserResponse> {
+
+        if (options.username.length <= 2){
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message : "length must be greater thant  2"
+                    }
+                ]
+            }
+        }
+
+        const user = await em.findOne(User, {username: options.username});
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: "That username doesn't exists",
+                    },
+                ]
+            };
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: "incorrect password",
+                    },
+                ]
+            };
+        }
+
+        req.session.userId = user.id;
+
+        return {user};
     }
 }
